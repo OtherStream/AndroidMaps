4f7c3b0a29cb2217aec879a049bd7a544181980f
OtherStream <OtherStream9042@gmail.com>
2026-04-07 22:26:00 -0600

    se le otorgaron permisos y se hizo las configuraciones del fragment

--- app/src/main/java/com/example/maps/LocationHelper.kt
+++ app/src/main/java/com/example/maps/LocationHelper.kt
@@ -1,2 +1,33 @@
 package com.example.maps
 
+import android.annotation.SuppressLint
+import android.content.Context
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+object LocationHelper {
+
+    @SuppressLint("MissingPermission")
+    suspend fun getCurrentLocation(context: Context): Pair<Double, Double>? =
+        suspendCancellableCoroutine { cont ->
+            val client = LocationServices.getFusedLocationProviderClient(context)
+            val cts = CancellationTokenSource()
+
+            client.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, cts.token)
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        cont.resume(Pair(location.latitude, location.longitude))
+                    } else {
+                        cont.resume(null)
+                    }
+                }
+                .addOnFailureListener {
+                    cont.resume(null)
+                }
+
+            cont.invokeOnCancellation { cts.cancel() }
+        }
+}
